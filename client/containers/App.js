6de41c30dabf39cdaf872d520efc1ca83f3861a0
Anton Stroinsky <anton.st@matomy.com>
2017-04-18 14:48:27 +0300

    added production config

---
+++ client/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../static/styles/libs.scss'
+import './App.scss'
+
+
+const App = (props) => {
+  return (
+    <div className="app">
+      <button className="button">puck</button>
+    </div>
+  )
+}
+
+export default App
--- client/index.js
+++ client/index.js
@@ -5,15 +5,15 @@ import { BrowserRouter as Router }  from 'react-router-dom'
 import configureStore               from './app-logic/store/store'
 const store = configureStore()
 
-import Root from './containers/Root'
+import Root from './containers/App'
 
-const App = () =>
-  <Provider store={ store }>
-    <Router history={ history }>
-      <Root />
-    </Router>
-  </Provider>
 
-ReactDOM.render(<App />, document.getElementById('react'))
+ReactDOM.render(
+    <Provider store={ store }>
+        <Router history={ history }>
+            <Root />
+        </Router>
+    </Provider>,
+    document.getElementById('react'))
 
 if (module.hot) module.hot.accept()
